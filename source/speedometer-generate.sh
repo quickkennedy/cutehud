741f9d964138523f4b4e8388d0bd093202cbc3db
Jack Kennedy <38009297+quickkennedy@users.noreply.github.com>
2022-05-23 09:48:49 -0400

    updated generate

--- source/speedometer-generate.sh
+++ source/speedometer-generate.sh
@@ -2,10 +2,11 @@
 # replace "white" with your background colour
 # replace "Metropolis-Bold" with your font of choice
 
-convert -auto-orient -background white -fill black -font Metropolis-Bold  -size 128x32 -gravity center -pointsize 32 label:0000 numbers/0000.tga
-
-for i in $(seq -w 9999)
+convert -auto-orient -background Transparent -fill White -font Montserrat-Bold  -size 128x32 -gravity center -pointsize 32 label:0 numbers/0000.tga
+n=1
+for i in $(seq -w 3500)
 do
-  convert -auto-orient -background white -fill black -font Metropolis-Bold -size 128x32 -gravity center -pointsize 32 label:$i numbers/$i.tga
+  convert -auto-orient -background Transparent -fill White -font Montserrat-Bold -size 128x32 -gravity center -pointsize 32 label:$n numbers/$i.tga
+  n=$((n + 1))
 done
 
