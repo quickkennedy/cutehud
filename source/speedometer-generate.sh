4548cbcf31cd49aa916ea13859ba65aa8aa50b2c
Jack Kennedy <38009297+quickkennedy@users.noreply.github.com>
2022-05-23 09:49:26 -0400

    Update speedometer-generate.sh

--- source/speedometer-generate.sh
+++ source/speedometer-generate.sh
@@ -1,6 +1,8 @@
-# replace "black" with your text colour
-# replace "white" with your background colour
-# replace "Metropolis-Bold" with your font of choice
+# REQUIRES IMAGEMAGICK IN THE SAME FOLDER WHEN RUN
+
+# replace "White" with your text colour
+# replace "Transparent" with your background colour
+# replace "Montserrat-Bold" with your font of choice
 
 convert -auto-orient -background Transparent -fill White -font Montserrat-Bold  -size 128x32 -gravity center -pointsize 32 label:0 numbers/0000.tga
 n=1
